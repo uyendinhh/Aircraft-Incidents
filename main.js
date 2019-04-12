8ccd38e3dcc2cf2521168dfb19463dd8d73ddc5b
Uyen Dinh <udinh3@gatech.edu>
2019-04-12 14:55:33 -0400

    added info when hovering

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ function start() {
     // g will contain geometry elements
     var g = svg.append("g");
 
+    var tooltip = d3.select("body").append("div") 
+        .attr("class", "tooltip")       
+        .style("opacity", 0);
+
+
     // Width and Height of the whole visualization
     // Set Projection Parameters
     var mapProjection = d3.geoMercator()
@@ -44,10 +49,9 @@ function start() {
     // Data and color scale
     var data = d3.map();
     var colorScale = d3.scaleThreshold()
-        .domain([0, 100, 1000, 10000, 100000, 500000000])
+        .domain([0, 20, 50, 100, 1000, 10000, 100000, 10000000, 500000000])
         .range(d3.schemeBlues[7]);
 
-    // Load external data and boot
     d3.queue()
         .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
         .defer(d3.csv, "./data/aircraft_incidents.csv", function (d) {
@@ -62,7 +66,7 @@ function start() {
     function ready(error, topo) {
 
         // Draw the map
-        svg.append("g")
+        var countries = svg.append("g")
             .selectAll("path")
             .data(topo.features)
             .enter()
@@ -74,10 +78,29 @@ function start() {
             // set the color of each country
             .attr("fill", function (d) {
                 d.total = data.get(d.properties.name) || 0;
-                return colorScale(1.3 * d.total);
-            });
+                return colorScale(d.total);
+            })
+            .on("mouseover", function(d) {    
+                tooltip.transition()    
+                .duration(200)    
+                .attr("transform", "translate(0, 100)")
+                .style("opacity", .9)    
+                tooltip.html(d.properties.name + " " + data.get(d.properties.name))
+                .attr("x", 200)
+                .style("left", (d3.event.pageX) + "px")   
+                .style("top", (d3.event.pageY) + "px"); 
+              })          
+              .on("mouseout", function(d) {   
+                tooltip.transition()    
+                .duration(700)    
+                .style("opacity", 0); 
+              })
+            .style("stroke", "white");
+        
+
     }
-    console.log(data);
+   
+
 
 
 
