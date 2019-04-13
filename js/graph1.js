d77f5f148535de8a6ab49127bfc70711145514c2
Jason Lee <jason.khn.lee@gmail.com>
2019-04-13 18:18:06 -0400

    added date slider to world map

--- js/graph1.js
+++ js/graph1.js
@@ -3,7 +3,7 @@ window.onload = start;
 function start() {
   // Width and Height of the whole visualization
   var width = 1250;
-  var height = 800;
+  var height = 650;
 
   // Create SVG
   var body = document.getElementById("graph1");
@@ -59,77 +59,105 @@ function start() {
       d3.json,
       "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
     )
-    .defer(d3.csv, "../data/aircraft_incidents.csv", function(d) {
-      var countryValue = data.has(d.Country)
-        ? parseInt(data.get(d.Country))
-        : 0;
-      data.set(d.Country, parseInt(d.Total_Fatal_Injuries) + countryValue);
-    })
-
     .await(ready);
 
-  var dataTime = d3.range(0, 10).map(function(d) {
-    return new Date(1995 + d, 10, 3);
-  });
-
-  var sliderTime = d3
-    .sliderBottom()
-    .min(d3.min(dataTime))
-    .max(d3.max(dataTime))
-    .step(1000 * 60 * 60 * 24 * 365)
-    .width(width - 200)
-    .tickFormat(d3.timeFormat("%Y"))
-    .tickValues(dataTime)
-    .default(new Date(1998, 10, 3))
-    .on("onchange", val => {
-      d3.select("p#value-time").text(d3.timeFormat("%Y")(val));
-    });
-
-  var gTime = d3
-    .select("div#slider-time")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", 100)
-    .append("g")
-    .attr("transform", "translate(30,30)");
-
-  gTime.call(sliderTime);
-
-  d3.select("p#value-time").text(d3.timeFormat("%Y")(sliderTime.value()));
-
   function ready(error, topo) {
-    // Draw the map
-    var countries = svg
-      .append("g")
-      .selectAll("path")
-      .data(topo.features)
-      .enter()
-      .append("path")
-      // draw each country
-      .attr("d", d3.geoPath().projection(mapProjection))
-      // set the color of each country
-      .attr("fill", function(d) {
-        d.total = data.has(d.properties.name) ? data.get(d.properties.name) : 0;
-        return colorScale(d.total);
-      })
-      .on("mouseover", function(d) {
-        tooltip
-          .transition()
-          .duration(200)
-          .attr("transform", "translate(0, 100)")
-          .style("opacity", 0.9);
-        tooltip
-          .html(d.properties.name + " " + (data.has(d.properties.name) ? data.get(d.properties.name) : 0))
-          .attr("x", 200)
-          .style("left", d3.event.pageX + "px")
-          .style("top", d3.event.pageY + "px");
-      })
-      .on("mouseout", function(d) {
-        tooltip
-          .transition()
-          .duration(700)
-          .style("opacity", 0);
-      })
-      .style("stroke", "white");
+    d3.csv("../data/aircraft_incidents.csv", function(csv) {
+      var currentYear = 1995;
+      function setData() {
+        data = d3.map();
+        for (var i = 0; i < csv.length; i++) {
+          var currentValue = data.has(csv[i].Country)
+            ? parseInt(data.get(csv[i].Country))
+            : 0;
+          var countryYear = new Date(csv[i].Event_Date).getFullYear();
+          var countryValue =
+            countryYear == currentYear
+              ? parseInt(csv[i].Total_Fatal_Injuries)
+              : 0;
+          data.set(csv[i].Country, countryValue + currentValue);
+        }
+      }
+      setData();
+      // Draw the map
+      var countries = svg
+        .append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+        // draw each country
+        .attr("d", d3.geoPath().projection(mapProjection))
+        // set the color of each country
+        .attr("fill", function(d) {
+          d.total = data.has(d.properties.name)
+            ? data.get(d.properties.name)
+            : 0;
+          return colorScale(d.total);
+        })
+        .on("mouseover", function(d) {
+          tooltip
+            .transition()
+            .duration(200)
+            .attr("transform", "translate(0, 100)")
+            .style("opacity", 0.9);
+          tooltip
+            .html(
+              d.properties.name +
+                " " +
+                (data.has(d.properties.name) ? data.get(d.properties.name) : 0)
+            )
+            .attr("x", 200)
+            .style("left", d3.event.pageX + "px")
+            .style("top", d3.event.pageY + "px");
+        })
+        .on("mouseout", function(d) {
+          tooltip
+            .transition()
+            .duration(700)
+            .style("opacity", 0);
+        })
+        .style("stroke", "white");
+
+      function setFill() {
+        countries.attr("fill", function(d) {
+          d.total = data.has(d.properties.name)
+            ? data.get(d.properties.name)
+            : 0;
+          return colorScale(d.total);
+        });
+      }
+
+      var dataTime = d3.range(0, 22).map(function(d) {
+        return new Date(1995 + d, 10, 3);
+      });
+
+      var sliderTime = d3
+        .sliderBottom()
+        .min(d3.min(dataTime))
+        .max(d3.max(dataTime))
+        .step(1000 * 60 * 60 * 24 * 365)
+        .width(width - 200)
+        .tickFormat(d3.timeFormat("%Y"))
+        .tickValues(dataTime)
+        .default(new Date(1995, 10, 3))
+        .on("onchange", val => {
+          currentYear = val.getFullYear();
+          setData();
+          setFill();
+          console.log(data);
+          console.log(currentYear);
+        });
+
+      var gTime = d3
+        .select("div#slider-time")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", 100)
+        .append("g")
+        .attr("transform", "translate(30,30)");
+
+      gTime.call(sliderTime);
+    });
   }
 }
